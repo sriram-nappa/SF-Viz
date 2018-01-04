db62d2895ac7e09e7c56ac314d0ccdbc605d86e7
srirampoondichinappa <2k6sriram@gmail.com>
2018-01-03 21:11:04 -0700

    Render SF map and refactor

--- sfmuni/src/component/busMap.js
+++ sfmuni/src/component/busMap.js
@@ -1,32 +1,62 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
 
-import arteriesData from './utils/sfmaps/arteries'
-import freewaysData from './utils/sfmaps/freeways'
-import neighborhoodData from './utils/sfmaps/neighborhoods'
-import streetData from './utils/sfmaps/streets'
-import { geoMercator } from 'd3-geo';
+import {arteriesData, freewaysData, neighborhoodData, streetData} from '../utils/sfmaps/sf-map'
+import './busMap.css';
+
+let mapData = [arteriesData, freewaysData, neighborhoodData, streetData]
+let mapName = ['arteries', 'freeways', 'neighborhoods', 'streets']
+
+let mapProjection = null
+let width = window.innerWidth * 0.8 - 30
+let height = window.innerHeight - 30
+let SFcity = {
+    lon: 122.4417686,
+    lat: 37.7682044
+}
 
 class BusMapSF extends Component {
     constructor(props) {
         super(props)
-        this.mapProjection = d3.geoMercator().translate([0,0]).precision(0)
         
         this.state = {
-            vehiclePos: null
+            vehicle: null,
+            path: {}
         }
     }
 
+
     componentDidMount() {
         let map = d3.select('.map-container').append('svg').attr('width',width).attr('height',height)
-        this.mapProjection.fitSize([width,height, neighborhoodData])
-        const gPath = d3.geoPath().projection(this.mapProjection)
+        mapProjection = d3.geoMercator().scale(200000).translate([width*3/8, height/2]).rotate([SFcity.lon, 0]).center([0,SFcity.lat])
+        mapProjection.fitSize([width, height],neighborhoodData)
+        const gPath = this.pathWithProjection(mapProjection)
+        let dMap = []
+        for(let i=0; i<mapName.length; i++) {
+            dMap[i] = this.drawMap(mapName[i], mapData[i], map, gPath)
+        }
+
+        let vehicle = map.append('g')
+        this.setState({
+            path: gPath,
+            vehicle: vehicle
+        })
+    }
+
+    pathWithProjection(projection) {
+        return d3.geoPath().projection(projection)
+    }
+
+    drawMap(className, mapData, map, gPath) {
+        return map.append('g').selectAll('path')
+            .data(mapData.features).enter()
+            .append('path').attr('class', className)
+            .attr('d', gPath)
     }
 
     render() {
         return (
         <div className="map-container">
-            "hello"
         </div>
         );
     }
